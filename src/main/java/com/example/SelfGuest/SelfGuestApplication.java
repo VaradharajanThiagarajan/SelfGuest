9a0b5b62f0c1562fbb5ba80816e0efc1fcebe470
varadharajanthiagarajan <varadha.001@gmail.com>
2021-04-06 11:26:56 -0400

    initial commit

---
+++ src/main/java/com/example/SelfGuest/SelfGuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SelfGuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfGuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfGuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SelfGuest/SelfGuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SelfGuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfGuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
