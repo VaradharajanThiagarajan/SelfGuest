5f22d042ae91fcf509822904ef50dcb868845217
varadharajanthiagarajan <varadha.001@gmail.com>
2021-04-06 14:26:09 -0400

    commit for green - added the unit testing for guestbook service - fetchall method was tested

--- src/main/java/com/example/SelfGuest/GuestBookService.java
+++ src/main/java/com/example/SelfGuest/GuestBookService.java
@@ -2,6 +2,9 @@ package com.example.SelfGuest;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 
 public class GuestBookService {
@@ -18,6 +21,15 @@ public class GuestBookService {
                 new GuestBookEntity(guestBookdtoobj.getName(),guestBookdtoobj.getComments())
         );
 
+    }
+
+    public List<GuestBookDto> fetchAll() {
+        return guestBookRepos.findAll()
+                .stream()
+                .map(guestBookEntity -> {
+                    return new GuestBookDto(guestBookEntity.getName(),guestBookEntity.getComments());
+                })
+                .collect(Collectors.toList());
     }
 
 }
--- src/test/java/com/example/SelfGuest/SelfGuestServiceTest.java
+++ src/test/java/com/example/SelfGuest/SelfGuestServiceTest.java
@@ -5,7 +5,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SelfGuestServiceTest {
@@ -24,4 +29,26 @@ public class SelfGuestServiceTest {
                 new GuestBookEntity("Shyam","I am 35 years old")
         );
     }
+
+
+    @Test
+    void fetchAllTest() {
+        GuestBookEntity guestBookEntity1 = new GuestBookEntity("Suki","I am 25 years old");
+        GuestBookEntity guestBookEntity2 = new GuestBookEntity("Rema","I am 65 years old");
+        List<GuestBookEntity> listveri = new ArrayList<GuestBookEntity>();
+        when(mockRepository.findAll()).thenReturn(listveri);
+        GuestBookDto guestBookDtoObject1 = new GuestBookDto("Suki","I am 25 years old");
+        GuestBookDto guestBookDtoObject2 = new GuestBookDto("Rema","I am 65 years old");
+        List<GuestBookDto> listveri1 = new ArrayList<GuestBookDto>();
+        listveri.add(guestBookEntity1);
+        listveri.add(guestBookEntity2);
+
+        listveri1.add(guestBookDtoObject1);
+        listveri1.add(guestBookDtoObject2);
+
+        List<GuestBookDto> guestBookDtolist1 = subject.fetchAll();
+
+        assertThat(guestBookDtolist1).isEqualTo(listveri1);
+    }
+
 }
